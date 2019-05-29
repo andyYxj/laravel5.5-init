f552a1aa38a61f8d64f27864d107715d5c59ff5e
AndyYu <yuxianjun001@icloud.com>
2019-05-29 13:59:23 +0800

    用户权限add

---
+++ app/Http/Controllers/Api/SuperAdmin/UserPermissionController.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by yuxianjun001@icloud.com.
+ * User: wuchen
+ * Date: 2019/5/29
+ * Time: 13:41
+ */
+
+namespace App\Http\Controllers\Api\SuperAdmin;
+
+
+use App\Http\Controllers\Common\MyController;
+use App\Services\Common\PermissionService;
+use Illuminate\Http\Request;
+
+class UserPermissionController extends MyController
+{
+    private $service;
+    private $request;
+
+    public function __construct(Request $request)
+    {
+        //该字段 做为权限组的标识，按照模块来，基本原则为如果需要，将超管，b端，c端分别做不同的guardName划分，规定为：超管后台的super_admin,b端后台的位b_admin,c端的为c_admin
+        //不同的控制器对应不同的guardName，调用的服务层代码一致
+        $request->guardName = 'super_admin';
+        $this->request      = $request;
+        $this->service      = new PermissionService();
+    }
+
+    /**
+     * 新增角色
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function add()
+    {
+        return $this->service->add($this->request);
+    }
+
+}
